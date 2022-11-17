b3510560bea58e69d76f57316acb743305f49179
fcg92 <fi.castrogonzalez@gmail.com>
2022-11-16 22:21:04 -0300

    Entrega de tema 8

--- ejercicios_tema_8.java
+++ ejercicios_tema_8.java
@@ -1,34 +1,39 @@
-public class ejercicios_tema_8 {
+public class ejercicios_tema_8{
     public static void main(String[] args) {
-        int numeroIf = 100;
-        int numeroWhile = 2;
 
-        if (numeroIf == 0) {
-            System.out.println(numeroIf+" es un numero igual a 0");            
-        } else if (numeroIf < 0) {
-            System.out.println(numeroIf+" es un numero negativo");           
-        } else if (numeroIf > 0) {
-            System.out.println(numeroIf+" es un numero positivo");;
-        }
+        Persona persona1 = new Persona();
 
+        persona1.setEdad(20);;
+        persona1.setNombre("Felip");;
+        persona1.setTelefono(75485468);
 
-
-        Coche coche = new Coche();
-        System.out.println(coche.numeroDePuertas);
-
+        System.out.println(persona1.getNombre());
+        System.out.println(persona1.getEdad());
+        System.out.println(persona1.getTelefono());
     }
 }
 
-class Coche {
-    int numeroDePuertas;
-    int velocidadMaxima;
+class Persona{
+    private int edad;
+    private String nombre;
+    private int telefono;
 
-    public Coche(){
-        numeroDePuertas =5;
+    public void setEdad(int edad){
+        this.edad = edad;
     }
-
-    public void acelerar(){
-        velocidadMaxima=+15;
+    public void setNombre(String nombre){
+        this.nombre = nombre;
     }
-
+    public void setTelefono(int telefono){
+        this.telefono = telefono;
+    }
+    public int getEdad(){
+        return this.edad;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    public int getTelefono(){
+        return this.telefono;
+    }
 }
